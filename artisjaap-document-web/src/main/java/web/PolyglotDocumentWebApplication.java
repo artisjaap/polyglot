ced6281c22a727b28850d6d6db61d113e1bbff0e
artisjaap <35543122+artisjaap@users.noreply.github.com>
2018-11-20 09:37:54 +0100

    document web

---
+++ artisjaap-document-web/src/main/java/web/PolyglotDocumentWebApplication.java
@@ -0,0 +1,17 @@
+package be.artisjaap.polyglot.document.web;
+
+import be.artisjaap.polyglot.PolyglotApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class PolyglotDocumentWebApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(PolyglotApplication.class)
+                //.sources(PolyglotDocumentApplication.class)
+                .build()
+                .run(args);
+    }
+}
