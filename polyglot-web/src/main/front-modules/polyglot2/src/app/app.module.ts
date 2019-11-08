099e538ddaf08b1050c9d2b1e6d90b9cd46bb37c
Stijn <stijn.coene@aclvb.be>
2019-11-08 09:47:36 +0100

    add ngrx

--- polyglot-web/src/main/front-modules/polyglot2/src/app/app.module.ts
+++ polyglot-web/src/main/front-modules/polyglot2/src/app/app.module.ts
@@ -6,6 +6,10 @@ import {CoreModule} from "./core/core.module";
 import {RouterModule} from "@angular/router";
 import {HttpClientModule} from "@angular/common/http";
 import {PolyglotCommonModule} from "./polyglot-common/polyglot-common.module";
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,15 @@ import {PolyglotCommonModule} from "./polyglot-common/polyglot-common.module";
     BrowserModule,
    // HttpClientModule,
     PolyglotCommonModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   exports: [RouterModule],
   providers: [
