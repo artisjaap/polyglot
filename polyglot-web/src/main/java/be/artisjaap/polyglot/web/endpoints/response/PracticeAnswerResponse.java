b419f5de45e04f5d6577ec2df697e2d5498192b3
coene.stijn@gmail.com <mylittleponyisamazing!>
2020-01-26 10:13:30 +0100

    fix practice answer response

--- polyglot-web/src/main/java/be/artisjaap/polyglot/web/endpoints/response/PracticeAnswerResponse.java
+++ polyglot-web/src/main/java/be/artisjaap/polyglot/web/endpoints/response/PracticeAnswerResponse.java
@@ -1,4 +1,16 @@
 package be.artisjaap.polyglot.web.endpoints.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class PracticeAnswerResponse   {
+    private String translationId;
+    private String userId;
+    private String languagePairId;
+    private String question;
+    private String givenAnswer;
+    private String expectedAnswer;
+    private Boolean correctAnswer;
 }
